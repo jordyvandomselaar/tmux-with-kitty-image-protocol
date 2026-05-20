626c29c2cfa5816b28ff2c98511991e76346ffab
Jordy van Domselaar <jordy@jordy.app>
2026-05-20 04:49:33 +0200

    Add kitty APC regression test

---
+++ regress/kitty-apc.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Kitty graphics APCs must be handled before generic APC title handling.
+
+PATH=/bin:/usr/bin
+TERM=screen
+
+[ -z "$TEST_TMUX" ] && TEST_TMUX=$(readlink -f ../tmux)
+TMUX="$TEST_TMUX -Ltest"
+CONF=$(mktemp)
+TMP=$(mktemp)
+trap "$TMUX kill-server 2>/dev/null; rm -f $CONF $TMP" 0 1 15
+
+$TMUX kill-server 2>/dev/null
+printf 'set -g allow-set-title on\n' >$CONF
+
+$TMUX -f$CONF new -d 'sleep 1'
+case "$($TMUX display -p '#{image_support}')" in
+*kitty*) ;;
+*) exit 0 ;;
+esac
+$TMUX kill-server 2>/dev/null
+
+test_apc() {
+	$TMUX kill-server 2>/dev/null
+	$TMUX -f$CONF new -d "printf '$1'; sleep 1"
+	sleep 0.5
+
+	$TMUX capturep -pS0 >$TMP || exit 1
+	grep -q "$2" $TMP || exit 1
+
+	title=$($TMUX display -p '#{pane_title}')
+	case "$title" in
+	*"$3"*) exit 1 ;;
+	esac
+}
+
+test_apc '\033_Ga=q,t=d,f=24,s=1,v=1;AAAA\033\\after-query\n' \
+    'after-query' 'Ga=q'
+test_apc '\033_Gbad\033\\after-malformed\n' 'after-malformed' 'Gbad'
+
+$TMUX kill-server 2>/dev/null
+
+exit 0
